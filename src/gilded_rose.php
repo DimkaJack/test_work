64c94e2ccf9364e696673e3381bad2c24fbfd305
dimkajack <drdouble@mail.ru>
2018-05-28 11:24:32 +0400

    * отрефакторил код  * добавил комментарии

--- src/gilded_rose.php
+++ src/gilded_rose.php
@@ -1,6 +1,20 @@
 <?php
 
+/**
+ * Class ProductFactory
+ *
+ * Имеет смысл для таких задач использовать базу данных, но в задаче не было это указано.
+ * Первоначально хотел использовать массивы, но вышло много кривого кода.
+ * Поэтому использовал фабрику, создавать абстракции не стал, т.к. кажутся здесь избыточными
+ */
 class ProductFactory{
+
+    /**
+     * Создание нужного класса на основе имени продукта.
+     *
+     * @param Item $item
+     * @return AgedBrie|Backstage|Conjured|BaseQualityUpdater|Sulfuras
+     */
     public function create(Item $item)
     {
         switch ($item->name){
@@ -17,23 +31,60 @@ class ProductFactory{
                 return new Conjured($item);
 
             default:
-                return new Product($item);
+                return new BaseQualityUpdater($item);
         }
     }
 }
 
-class Product{
+
+/**
+ * Базовый класс для изменения качества и срока хранения
+ *
+ * Class Product
+ */
+class BaseQualityUpdater{
+
     const QUALITY_ITERATOR = 1,
         SELL_IN_ITERATOR = 1,
         MAX_QUALITY = 50,
         MIN_QUALITY = 0;
 
     protected $item;
+
+    /**
+     * Множитель на который увеличиваем QUALITY_ITERATOR
+     * @var int
+     */
     public $quality_iterator = 1;
+
+    /**
+     * Множитель на который увеличиваем SELL_IN_ITERATOR
+     * @var int
+     */
     public $sell_in_iterator = 1;
+
+    /**
+     * Если true - увеличиваем качество, если false - уменьшаем
+     * @var bool
+     */
     public $iterate = false;
+
+    /**
+     * Множитель для просроченных товаров
+     * @var int
+     */
     public $passed_quality_iterator = 2;
+
+    /**
+     * Множитель для просроченных товаров
+     * @var int
+     */
     public $passed_sell_in_iterator = 1;
+
+    /**
+     * Аналогично $iterate, но для для просроченных товаров
+     * @var bool
+     */
     public $passed_iterate = false;
 
     public function __construct(Item $item)
@@ -41,6 +92,12 @@ class Product{
         $this->item = $item;
     }
 
+    /**
+     * Определяем просрок и получаем необходимые множители
+     *
+     * @param $sell_in
+     * @return array
+     */
     public function getIterators($sell_in)
     {
         if( $sell_in >= 0){
@@ -58,43 +115,52 @@ class Product{
         ];
     }
 
-    public function update_quality()
+    /**
+     * Обновляем качество и срок
+     */
+    public function update()
     {
-        list($quality_iterator, $sell_in_iterator, $iterate) = $this->getIterators($this->item->sell_in);
+        list($quality_iterator, $sell_in_iterator, $is_iterate) = $this->getIterators($this->item->sell_in);
 
         $iterator = $quality_iterator * self::QUALITY_ITERATOR;
-        if( $iterate ){
+        if( $is_iterate ){
             $this->item->quality += $iterator;
         }else{
             $this->item->quality -= $iterator;
         }
 
         $iterator = $sell_in_iterator * self::SELL_IN_ITERATOR;
-
         $this->item->sell_in -= $iterator;
 
+        //Качество не должно быть меньше минимума
         if( $this->item->quality < self::MIN_QUALITY ){
             $this->item->quality = self::MIN_QUALITY;
         }
 
+        //Качество не должно быть больше максимума
         if( $this->item->quality > self::MAX_QUALITY ){
             $this->item->quality = self::MAX_QUALITY;
         }
     }
 }
 
-class AgedBrie extends Product {
+
+class AgedBrie extends BaseQualityUpdater {
     public $iterate = true;
     public $passed_quality_iterator = 1;
     public $passed_iterate = true;
 }
 
-class Backstage extends Product {
+
+class Backstage extends BaseQualityUpdater {
     public $iterate = true;
 
-    public function update_quality()
+    /**
+     * Переопределяем родительский метод, т.к. у Backstage дополнительные условия
+     */
+    public function update()
     {
-        parent::update_quality();
+        parent::update();
 
         if ($this->item->sell_in < 11) {
             if ($this->item->quality < self::MAX_QUALITY) {
@@ -109,17 +175,20 @@ class Backstage extends Product {
     }
 }
 
-class Sulfuras extends Product {
+
+class Sulfuras extends BaseQualityUpdater {
     public $quality_iterator = 0;
     public $sell_in_iterator = 0;
     public $passed_quality_iterator = 0;
     public $passed_sell_in_iterator = 0;
 }
 
-class Conjured extends Product {
+
+class Conjured extends BaseQualityUpdater {
     public $quality_iterator = 2;
     public $passed_quality_iterator = 4;
 }
+
 
 class GildedRose {
 
@@ -132,9 +201,9 @@ class GildedRose {
     function update_quality() {
         foreach ($this->items as $item) {
 
-            $product = (new ProductFactory())->create($item);
+            $quality_updater = (new ProductFactory())->create($item);
 
-            $product->update_quality();
+            $quality_updater->update();
 
         }
     }
